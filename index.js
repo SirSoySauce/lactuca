0f88fa7e096c8a76195e8c2f0fa29eb1f50b908e
Ivo Cavelti <git@cavelti.dev>
2023-12-16 10:13:14 +0100

    Refactor code to remove jQuery dependency
    This change removes any unused parts in the code and any dependency on
    the jQuery library. Avoids unnecessary bloat of the code.

--- index.js
+++ index.js
@@ -1,109 +1,65 @@
-var Typer = {
-  text: '',
-  accessCountimer: null,
-  index: 0,
-  speed: 2,
-  file: '',
-  accessCount: 0,
-  deniedCount: 0,
-  init: function () {
-    accessCountimer = setInterval(function () {
-      Typer.updLstChr();
-    }, 500);
-    $.get(Typer.file, function (data) {
-      Typer.text = data;
-      Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-    });
-  },
+const consoleContent = document.getElementById('console-content');
+const dynamicConsoleNative = document.getElementById('dynamic-console')
 
-  content: function () {
-    return $('#console').html();
-  },
+const Typer = {
+    text: '',
+    accessCountimer: null,
+    index: 0,
+    speed: 3,
+    init: function () {
+        this.accessCountimer = setInterval(function () {
+            Typer.updLstChr();
+        }, 500);
+        Typer.text = consoleContent.innerHTML;
+        Typer.text = Typer.text.slice(0, Typer.text.length - 1).trim();
+    },
 
-  write: function (str) {
-    $('#console').append(str);
-    return false;
-  },
+    content: function () {
+        return dynamicConsoleNative.innerHTML;
+    },
 
-  addText: function (key) {
-    if (key.keyCode == 18) {
-      Typer.accessCount++;
+    append: function (str) {
+        dynamicConsoleNative.innerHTML += str;
+        return false;
+    },
 
-      if (Typer.accessCount >= 3) {
-        Typer.makeAccess();
-      }
-    } else if (key.keyCode == 20) {
-      Typer.deniedCount++;
+    write: function (str) {
+        dynamicConsoleNative.innerHTML = str;
+        return false;
+    },
 
-      if (Typer.deniedCount >= 3) {
-        Typer.makeDenied();
-      }
-    } else if (key.keyCode == 27) {
-      Typer.hidepop();
-    } else if (Typer.text) {
-      var cont = Typer.content();
-      if (cont.substring(cont.length - 1, cont.length) == '|')
-        $('#console').html(
-          $('#console')
-            .html()
-            .substring(0, cont.length - 1),
-        );
-      if (key.keyCode != 8) {
+
+    addText: function () {
+        const cont = Typer.content();
+        if (cont.substring(cont.length - 1, cont.length) === '|') {
+            this.write(this.content().substring(0, cont.length - 1));
+        }
+
         Typer.index += Typer.speed;
-      } else {
-        if (Typer.index > 0) Typer.index -= Typer.speed;
-      }
-      var text = Typer.text.substring(0, Typer.index);
-      var rtn = new RegExp('\n', 'g');
+        const text = Typer.text.substring(0, Typer.index);
 
-      $('#console').html(text.replace(rtn, '<br/>'));
-      window.scrollBy(0, 50);
-    }
+        this.write(text)
+        window.scrollBy(0, 50);
+    },
 
-    if (key.preventDefault && key.keyCode != 122) {
-      key.preventDefault();
-    }
+    updLstChr: function () {
+        const cont = this.content();
 
-    if (key.keyCode != 122) {
-      // otherway prevent keys default behavior
-      key.returnValue = false;
-    }
-  },
-
-  updLstChr: function () {
-    var cont = this.content();
-
-    if (cont.substring(cont.length - 1, cont.length) == '|')
-      $('#console').html(
-        $('#console')
-          .html()
-          .substring(0, cont.length - 1),
-      );
-    else this.write('|'); // else write it
-  },
+        if (cont.substring(cont.length - 1, cont.length) === '|') {
+            this.write(this.content().substring(0, cont.length - 1));
+        } else {
+            this.append('|');
+        }
+    },
 };
-
-function replaceUrls(text) {
-  var http = text.indexOf('http://');
-  var space = text.indexOf('.me ', http);
-
-  if (space != -1) {
-    var url = text.slice(http, space - 1);
-    return text.replace(url, '<a href="' + url + '">' + url + '</a>');
-  } else {
-    return text;
-  }
-}
-
-Typer.speed = 3;
-Typer.file = 'ivo_cavelti/about_me/ivo-cavelti.txt';
 Typer.init();
 
-var timer = setInterval('t();', 30);
-function t() {
-  Typer.addText({ keyCode: 123748 });
+const timer = setInterval('t();', 30);
 
-  if (Typer.index > Typer.text.length) {
-    clearInterval(timer);
-  }
+function t() {
+    Typer.addText();
+
+    if (Typer.index > Typer.text.length) {
+        clearInterval(timer);
+    }
 }
